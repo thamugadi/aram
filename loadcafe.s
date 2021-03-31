fe05ad3ae805e0f15b14964ab4a053e369286112
thamugadi <22577625+thamugadi@users.noreply.github.com>
2021-03-31 22:16:57 +0100

    Create loadcafe.s

---
+++ loadcafe.s
@@ -0,0 +1,39 @@
+bits 16
+org 0xcafe
+
+cli
+lgdt [gdtptr]
+mov eax, cr0
+or al, 1
+mov cr0, eax
+jmp protected
+protected:
+mov ax, 0x10
+mov ds, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
+mov ss, ax
+mov sp, 0xbeef
+jmp 0x8:0x8000
+
+gdt:
+db 0,0,0,0,0,0,0,0
+gdt_cs:
+dw 0xFFFF
+dw 0x0000
+db 0x00
+db 0b10011010
+db 0b11001111
+db 0x00
+gdt_ds:
+dw 0xFFFF
+dw 0x0000
+db 0x00
+db 0b10010010
+db 0b11001111
+db 0x00
+gdtend:
+gdtptr:
+dw gdtend - gdt - 1
+dd gdt
