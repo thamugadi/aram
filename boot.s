ce601d441f34a2dff38ff506acfeab53001d60fd
aramya <22577625+thamugadi@users.noreply.github.com>
2021-09-06 23:05:53 +0100

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,36 +1,28 @@
-bits 16
-
+.intel_syntax noprefix
+.code16
 mov ah, 0
 int 0x13
-
 mov ax, 0
 mov es, ax
-
 mov ah, 2
 mov al, 1
 mov ch, 0
 mov cl, 2
 mov dh, 0
-
 mov bx, 0x1000
-int 0x13 ;;boot1 loaded
-
+int 0x13
 mov ah, 2
 mov al, 1
 mov ch, 0
 mov cl, 3
 mov dh, 0
-
 mov bx, 0x1FFF
-int 0x13 ;;boot2 loaded
-
+int 0x13
 mov ah, 2
 mov al, 128
 mov ch, 0
 mov cl, 4
 mov dh, 0
-
 mov bx, 0xCAFE
-int 0x13  ;;rest of the file loaded
-
-jmp 0:0x1000 ;;jump to boot1
+int 0x13 
+jmp 0:0x1000
