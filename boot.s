fdbc4d8dc8102c31d6a107a92f76e0d13058ac4a
aramya <22577625+thamugadi@users.noreply.github.com>
2021-09-08 02:18:10 +0100

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,25 +1,28 @@
 .intel_syntax noprefix
 .code16
+
 mov ah, 0
 int 0x13
+
 mov ax, 0
 mov es, ax
+
 mov ah, 2
-mov al, 1
+mov al, 4 ;; load 2 KB (boot1)
 mov ch, 0
 mov cl, 2
 mov dh, 0
 mov bx, 0x1000
 int 0x13
 mov ah, 2
-mov al, 1
+mov al, 2 ;; load 1 KB (boot2)
 mov ch, 0
 mov cl, 3
 mov dh, 0
 mov bx, 0x1FFF
 int 0x13
 mov ah, 2
-mov al, 128
+mov al, 255 ;; load up to 128 KB (kernel)
 mov ch, 0
 mov cl, 4
 mov dh, 0
