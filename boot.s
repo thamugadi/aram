b76533bcdf8e4b4372adeb796c2d21c7284308c0
aramya <22577625+thamugadi@users.noreply.github.com>
2021-09-08 02:18:55 +0100

    Update boot.s

--- boot.s
+++ boot.s
@@ -8,21 +8,21 @@ mov ax, 0
 mov es, ax
 
 mov ah, 2
-mov al, 4 ;; load 2 KB (boot1)
+mov al, 4 / load 2 KB (boot1)
 mov ch, 0
 mov cl, 2
 mov dh, 0
 mov bx, 0x1000
 int 0x13
 mov ah, 2
-mov al, 2 ;; load 1 KB (boot2)
+mov al, 2 / load 1 KB (boot2)
 mov ch, 0
 mov cl, 3
 mov dh, 0
 mov bx, 0x1FFF
 int 0x13
 mov ah, 2
-mov al, 255 ;; load up to 128 KB (kernel)
+mov al, 255 / load up to 128 KB (kernel)
 mov ch, 0
 mov cl, 4
 mov dh, 0
