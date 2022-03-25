be564ae83f87a22c58daa5193a7b4a504968461d
thamugadi <qirimhangli@gmail.com>
2022-03-25 17:29:48 +0100

    32-bit paging macros

--- macro.s
+++ macro.s
@@ -1,23 +1,19 @@
 .intel_syntax noprefix
 .macro .GDT_ENTRY limit base access flags
-
 .word \limit
 .word \base
 .byte \base >> 16
 .byte \access
 .byte \limit >> 16 & 0x0F | \flags << 4
 .byte \base >> 24
-
 .endm
 
 .macro .IDT_ENTRY offset selector access
-
 .short \offset & 0x0000FFFF
 .short \selector
 .byte 0
 .byte  \access
 .short \offset >> 16
-
 .endm
 
 .macro .CR3_32BIT page_dir_addr pcd pwt
